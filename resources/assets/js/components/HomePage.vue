b8167d4aef1f9ad1bd7c9a791e1f97b84f0928c7
Julio Alberto T <jultum88@gmail.com>
2018-01-24 01:00:04 -0400

    compartiendo datos

--- resources/assets/js/components/HomePage.vue
+++ resources/assets/js/components/HomePage.vue
@@ -11,18 +11,14 @@
 
 <script>
 	
-	
-	// import axios from 'axios';
-	// let serverData = JSON.parse(window.vuebnb_server_data);
-	// let listing_groups = groupByCountry(serverData.listings);
-	import routeMixin from '../route-mixin';
 	import ListingSummaryGroup from './ListingSummaryGroup.vue';
 	import { groupByCountry } from '../helpers';
 
 	export default {
-		mixins: [ routeMixin ],
-		data() {
-			return { listing_groups: [] };
+		computed: {
+			listing_groups() {
+				return groupByCountry(this.$store.state.listing_summaries);
+			}
 		},
 		methods: {
 			assignData({ listings }){
@@ -32,49 +28,10 @@
 		components: {
 			ListingSummaryGroup
 		},
-		// beforeRouteEnter(to, from, next) {
-		// 	let serverData = JSON.parse(window.vuebnb_server_data);
-		// 	if (to.path === serverData.path) {
-		// 		let listing_groups = groupByCountry(serverData.listings);
-		// 		next(component =>  component.listing_groups = listing_groups);
-		// 	} else { 
-		// 		axios.get(`/api/`).then(({ data }) => {
-		// 			let listing_groups = groupByCountry(data.listings);
-		// 			next(component => component.listing_groups = listing_groups);
-		// 		});
-		// 	}
-		// }
+		
 	}
 </script>
 
 <style>
-	/*.home-container {
-		margin: 0 auto;
-		padding: 0 25px;
-	}
 
-	@media (min-width: 1131px) {
-		.home-container {
-			width: 1080px;
-		}
-	}
-
-	.listing-summary-group {
-		padding-bottom: 20px;
-	}
-
-	.listing-summaries {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		overflow: hidden;
-	}
-
-	.listing-summaries > .listing-summary {
-		margin-right: 15px;
-	}
-
-	.listing-summaries > .listing-summary:last-child {
-		margin-right: 0;
-	}*/
 </style>
--- resources/assets/js/components/ListingPage.vue
+++ resources/assets/js/components/ListingPage.vue
@@ -1,27 +1,27 @@
 <template>
 	<div>
-		<header-image v-if="images[0]" :image-url="images[0]" @header-clicked="openModal">
+		<header-image v-if="listing.images[0]" :image-url="listing.images[0]" @header-clicked="openModal" :id="listing.id">
   	</header-image>
   	<div class="listing-container">
-    	<div class="heading">]
-      	<h1>{{ title }}</h1>
-      	<p>{{ address }}</p>
+    	<div class="heading">
+      	<h1>{{ listing.title }}</h1>
+      	<p>{{ listing.address }}</p>
     	</div>
     	<hr>
     	<div class="about">
       	<h3>About this listing</h3>
       	<expandable-text>
-        	{{ about }}
+        	{{ listing.about }}
       	</expandable-text>
     	</div>
     	<div class="lists">
-      	<feature-list title="Amenities" :items="amenities">
+      	<feature-list title="Amenities" :items="listing.amenities">
         	<template  slot-scope="amenity">
           	<i class="fa fa-lg" v-bind:class="amenity.icon"></i>
           	<span>{{ amenity.title }}</span>
         	</template>
       	</feature-list>
-      	<feature-list title="Prices" :items="prices">
+      	<feature-list title="Prices" :items="listing.prices">
         	<template slot-scope="price">
           	{{ price.title }}: <strong>{{ price.value }}</strong>
         	</template>
@@ -29,36 +29,30 @@
     	</div>
   	</div>
   	<modal-window ref="imagemodal">
-    	<image-carousel :images="images"></image-carousel>
+    	<image-carousel :images="listing.images"></image-carousel>
   	</modal-window>
 	</div>
 </template>
 <script>
 
 	import { populateAmenitiesAndPrices } from '../helpers';
-  // let serverData = JSON.parse(window.vuebnb_server_data);
-	// let model = populateAmenitiesAndPrices(serverData.listing);
-
+  
 	import ImageCarousel from './ImageCarousel.vue';
 	import ModalWindow from './ModalWindow.vue';
 	import HeaderImage from './HeaderImage.vue';
 	import FeatureList from './FeatureList.vue';
 	import ExpandableText from './ExpandableText.vue';
 
-  import routeMixin from '../route-mixin';
+
 
 	export default {
-    mixins: [ routeMixin ],
-		data() {
-			return {
-        title: null,
-        about: null,
-        address: null,
-        amenities: [],
-        prices: [],
-        images: []
+    computed: {
+      listing() {
+        return populateAmenitiesAndPrices(
+            this.$store.getters.getListing(this.$route.params.listing)
+          );  
       }
-		},
+    },  
 		methods: {
       assignData({ listing }) {
         Object.assign(this.$data, populateAmenitiesAndPrices(listing));
